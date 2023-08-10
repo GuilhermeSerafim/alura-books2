617b00cbe2f661e70d291d96dec73e92d327c0a4
GuilhermeSerafim <guilerstudies@gmail.com>
2023-08-09 22:10:40 -0300

    Fazendo mensagens personalizadas para erro, e entendendo sobre catch

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-    var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCep = fetch('https://viacep.com.br/ws/01001250/json/')
     .then(resposta => resposta.json())
-    .then(r => console.log(r));
+    .then(r => {
+        if(r.erro) {
+            throw Error("Esse cep não existe!")
+        } else {
+        console.log(r)
+        }
+    })
+    .catch(erro => console.log(erro));
 
     console.log(consultaCep);
