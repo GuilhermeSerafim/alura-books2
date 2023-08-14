8e7159f6f3c3f42a52ee67b32336e8da6e44d067
GuilhermeSerafim <guilerstudies@gmail.com>
2023-08-14 16:11:15 -0300

    Imprimindo conjunto de ceps com promise.all no console

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 //Código de fácil leitura e assíncrono
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultaCep = await fetch('https://viacep.com.br/ws/01001250/json/');
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCepConvertida = await consultaCep.json();
 
         if (consultaCepConvertida.erro) {
             throw Error('Esse CEP não existe');
         };
 
-        console.log(consultaCepConvertida);
+        return consultaCepConvertida;
     } catch (erro) {
         console.log(erro);
     };
 };
 
-buscaEndereco();
+let ceps = ['01001000', '05776340', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
